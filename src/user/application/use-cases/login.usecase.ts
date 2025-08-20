df39e178127f6ff5b487f4639d34b0c59bb7c73e
hieu26102004 <136700894+hieu26102004@users.noreply.github.com>
2025-08-20 21:54:59 +0700

    refactor: remove prefix, return exception, safety type

--- src/user/application/use-cases/login.usecase.ts
+++ src/user/application/use-cases/login.usecase.ts
@@ -1,32 +1,32 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, UnauthorizedException } from '@nestjs/common';
 import type { UserRepository } from '../../domain/repositories/user.repository';
 import { USER_REPOSITORY } from '../../domain/repositories/user-repository.token';
 import { PASSWORD_HASHER } from '../../domain/repositories/password-hasher.token';
 import type { PasswordHasher } from '../../domain/repositories/password-hasher';
 import type { UserEntity } from '../../domain/entities/user.entity';
-import type { JwtService as JwtServiceInterface } from 'src/shared/interfaces/jwt.service';
 import { JWT_SERVICE } from 'src/shared/interfaces/jwt-service.token';
+import { JwtService } from 'src/shared/infrastructure/jwt.service';
 
 @Injectable()
 export class LoginUseCase {
   constructor(
     @Inject(USER_REPOSITORY) private readonly userRepository: UserRepository,
     @Inject(PASSWORD_HASHER) private readonly passwordHasher: PasswordHasher,
-    @Inject(JWT_SERVICE) private readonly jwtService: JwtServiceInterface,
+    @Inject(JWT_SERVICE) private readonly jwtService: JwtService,
   ) {}
 
-  async execute(identifier: string, password: string): Promise<{ user: Partial<UserEntity>; accessToken: string } | null> {
+  async execute(identifier: string, password: string): Promise<{ user: Partial<UserEntity>; accessToken: string }> {
     const isEmail = /.+@.+\..+/.test(identifier);
     const user = isEmail
       ? await this.userRepository.findByEmail(identifier)
       : await this.userRepository.findByUsername(identifier);
-    if (!user) return null;
+    if (!user) throw new UnauthorizedException('Invalid credentials');
 
     const isMatch = await this.passwordHasher.compare(password, user.password);
-    if (!isMatch) return null;
+    if (!isMatch) throw new UnauthorizedException('Invalid credentials');
 
     const accessToken = this.jwtService.sign({ sub: user.id, email: user.email, username: user.username });
-    const { password: _pw, otp, otpExpiresAt, ...safeUser } = user as any;
+    const { password: _password, otp, otpExpiresAt, ...safeUser } = user;
     return { user: safeUser, accessToken };
   }
 }
